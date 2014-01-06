3546f9b76d2d1332330da865832e0b457b229d40
siggip <siggi@tgwnn.de>
2014-01-06 01:22:03 +0100

    added AMD and non AMD functionalities, Template loading fixed # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch b-2013-10-13-init # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	new file:   dev/assets/css/app.css #	new file:   dev/assets/css/bootstrap.css #	new file:   dev/assets/img/glyphicons-halflings-white.png #	new file:   dev/assets/img/glyphicons-halflings.png #	new file:   dev/assets/js/.jshintrc #	new file:   dev/assets/js/skeleton.js #	new file:   dev/assets/js/vendor/backbone.js #	new file:   dev/assets/js/vendor/backbone.marionette.js #	new file:   dev/assets/js/vendor/bootstrap.js #	new file:   dev/assets/js/vendor/jquery.js #	new file:   dev/assets/js/vendor/json2.js #	new file:   dev/assets/js/vendor/underscore.js #	new file:   dev/index.html #	new file:   dev/json/formfields.json #

--- dev/assets/js/skeleton.js
+++ dev/assets/js/skeleton.js
@@ -5,80 +5,18 @@
  * Time: 13:46
  * To change this template use File | Settings | File Templates.
  */
-(function($) {
-    'use strict';
+/*global window*/
+(function (factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register skeleton module.
+    define(['skeleton/formbuilder'], factory);
+  } else {
+    // Browser globals
+    window.Skeleton = factory(window.formbuilder);
+  }
+}(function(formbuilder) {
+  'use strict';
+  return {'FormBuilder': formbuilder};
+}));
 
-    var defaultConfig = {
-        'url': '/json/formfields.json',
-        'type':'json'
-    };
-
-    if(typeof Marionette !== 'object'){
-        throw new Error('Marionette is undefined');
-    }
-    if(typeof _ !== 'function'){
-        throw new Error('underscore is undefined');
-    }
-
-    // make shortcut for Skeleton global
-    window.SK = function(){
-        this.options = $.extend(defaultConfig,arguments[0]);
-        this.App = new Marionette.Application();
-        this.App.addRegions({
-            mainRegion: '#main-region'
-        });
-        // field model defaults
-        this.App.Field = Backbone.Model.extend({
-            defaults: {
-                'id': 0,
-                'name': 'unknown',
-                'label': 'unknown',
-                'group': 'unknown',
-                'mandatory': true,
-                'type': 'text'
-            }
-        });
-        this.App.FieldView = Marionette.ItemView.extend({
-            template: '#field-template' // really needed
-        });
-        this.App.FieldListView = Marionette.CollectionView.extend({
-            tagName: 'div',
-            itemView: this.App.FieldView
-        });
-        this.App.FieldsCollection = Backbone.Collection.extend({
-            model: this.App.Field
-        });
-        this.App.Fields = new this.App.FieldsCollection();
-        return this;
-    };
-    // create Form and load field definitions from source
-    window.SK.prototype.create = function(options){
-        console.log('create Fields');
-        if(typeof options !== 'object'){
-            throw new Error('missing options parameter');
-        }
-        this.options = $.extend(this.options, options);
-        switch(this.options.type){
-        case 'json':
-            this.loadJSON();
-            break;
-        }
-    };
-    window.SK.prototype.loadJSON = function(){
-        var that = this;
-        console.log(that.App.Fields);
-        $.getJSON(this.options.url, function(data){
-            $.each(data, function(idx, list){
-                console.log('creating '+idx+': '+list.length);
-                $.each(list, function(idx2, modeldef){
-                    var _m = new that.App.Field(modeldef);
-                    console.log(_m);
-                    that.App.Fields.add(_m);
-                    // creating the view
-
-                });
-            });
-            console.log(that.App.Fields);
-        });
-    };
-})(jQuery);
